fa13c1809a98194d408374378eeb48356707594b
thomas.schuster <thomas.schuster@Schuster-Surf4.fhres.ad.fh-pforzheim.de>
2018-06-05 14:51:45 +0200

    added editable functionality to schedule

---
+++ src/main/java/de/hspf/sysdev/web/materialize/view/user/UserBean.java
@@ -0,0 +1,61 @@
+package de.hspf.sysdev.web.materialize.view.user;
+
+import de.hspf.sysdev.web.materialize.view.util.ViewContextUtil;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+
+/**
+ *
+ * @author thomas
+ */
+@Named(value = "loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+
+    private boolean loggedIn;
+    private String userName;
+    private String password;
+
+    public LoginBean() {
+        setLoggedIn(false);
+    }
+
+    public String authenticate() {
+        //TODO add real authentication mechanism
+        if (getUserName().equals("thomas")) {
+            setLoggedIn(true);
+            ViewContextUtil.getFacesContext().addMessage(null, new FacesMessage("Login sucessfull."));
+            return "index";
+        } else {
+            ViewContextUtil.getFacesContext().addMessage(null, new FacesMessage("Login failed, either username or password are wrong. You provided:" + getUserName()));
+            return "login";
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+}
